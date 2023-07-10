8d120b7962dae12ce35318888f989c1b04c7bb33
Raymond <raymond.yang121@outlook.com>
2023-07-10 19:53:43 -0400

    update dataset class with additional pre-processing

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import torchaudio
-import os
+import numpy as np
 from dataclasses import dataclass
 
 from .constants import Columns, Emotions
@@ -11,21 +11,27 @@ from .constants import Columns, Emotions
 
 class AudioEmotionDataset(Dataset):
     # change so that the csv is passed in as a dataframe
-    def __init__(self, csv_dataframe, n_mels=64, sample_rate=16000,
-                transform=None, test_mode=False) -> None:
+    def __init__(self, csv_dataframe, n_mels=64, sample_rate=16000, n_fft=400, max_audio_length_sec=3.65, device='cpu',
+                test_mode=False) -> None:
 
         self.dataframe = csv_dataframe
         self.labels = self.dataframe[Columns.EMOTIONS].unique()
 
-        if transform == None:
-            # Create a MelSpectrogram transform by default
-            self.transform = torchaudio.transforms.MelSpectrogram(
-                sample_rate=sample_rate,
-                n_mels=n_mels,
-                n_fft=400
-            )
-        else:
-            self.transform = transform # use the transform passed in
+        # Create a MelSpectrogram transform by default
+        self.transform = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_mels=n_mels,
+            n_fft=400,
+        )
+        self.transform.to(device)
+
+        self.n_mels = n_mels
+        self.sample_rate = sample_rate
+        self.n_fft = n_fft
+        self.hop_length = self.transform.hop_length
+        
+        self.max_timeframes  = self._get_max_timeframes(max_audio_length_sec)
+        self.max_audio_length_sec = max_audio_length_sec
 
         self.test_mode = test_mode
 
@@ -38,7 +44,11 @@ class AudioEmotionDataset(Dataset):
 
         audio_file_path = self.dataframe.loc[idx, Columns.PATH]
         audio_length = self.dataframe.loc[idx, Columns.AUDIO_LENGTH]
-        waveform, sample_rate = torchaudio.load(audio_file_path)
+        waveform, waveform_sample_rate = torchaudio.load(audio_file_path)
+
+        waveform = self._resample_if_necessary(waveform, waveform_sample_rate)
+        waveform = self._mix_down_if_necessary(waveform)
+        waveform = self._truncate_or_pad_waveform(waveform)
 
         # Apply the transform
         input = self.transform(waveform)
@@ -51,3 +61,29 @@ class AudioEmotionDataset(Dataset):
             return input, label, waveform, audio_length, audio_file_path
         else:
             return input, label
+
+
+    def _get_max_timeframes(self, max_audio_length_sec):
+        # Estimate the maximum number of timeframes based on the audio lengths
+        max_timeframes = int(np.ceil(max_audio_length_sec * self.sample_rate / self.hop_length))
+        return max_timeframes
+    
+    def _resample_if_necessary(self, waveform, waveform_sample_rate):
+        if waveform_sample_rate != self.sample_rate:
+            waveform = torchaudio.transforms.Resample(waveform_sample_rate, self.sample_rate)(waveform)
+        return waveform
+    
+    def _mix_down_if_necessary(self, waveform):
+        if waveform.shape[0] > 1:
+            waveform = torch.mean(waveform, dim=0, keepdim=True)
+        return waveform
+    
+    def _truncate_or_pad_waveform(self, waveform):
+        max_samples = self.max_timeframes * self.hop_length
+        if waveform.shape[1] < max_samples:
+            # Pad waveform with zeros
+            waveform = torch.nn.functional.pad(waveform, (0, max_samples - waveform.shape[1]))
+        else:
+            # Truncate waveform
+            waveform = waveform[:, :max_samples]
+        return waveform
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,6 +5,9 @@ sys.path.insert(0, BASE_DIR) # Add src/ directory to python path so we can impor
 
 from unittest import TestCase, main
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa.display
 from src.utils.dataset import AudioEmotionDataset
 from src.utils.constants import Columns, Emotions, Datasets
 import src.utils.data_processing as data_processing
@@ -25,13 +28,60 @@ class AudiEmotionDatasetTestCase(TestCase):
 
         dataset = AudioEmotionDataset(csv_df, test_mode=True)
 
-        for i in range(len(dataset)):
+        for i in range(min(len(dataset), 30)):
             input, label, waveform, audio_length, audio_file_path = dataset[i]
             self.assertEqual(audio_file_path, csv_df.loc[i, Columns.PATH])
             self.assertEqual(Emotions().from_index(label.item()), csv_df.loc[i, Columns.EMOTIONS])
             self.assertEqual(audio_length, csv_df.loc[i, Columns.AUDIO_LENGTH])
 
-            # note: won't test mel_spec and waveform here. They will be tested by plotting them in the notebook
+            # note: won't test mel_spec and waveform here.
+        
+    def test_transform_padding(self):
+        # purpose: test that the transform is applied correctly
+
+        csv_df = data_processing.read_csv(
+            pd.read_csv(
+                os.path.join(BASE_DIR, 'data/metadata/dataset.csv'),
+                index_col=False
+            ),
+            BASE_DIR
+        )
+
+        dataset = AudioEmotionDataset(csv_df, test_mode=True)
+        print('\nmax_audio_length_sec', dataset.max_audio_length_sec)
+        print('max_timeframes', dataset.max_timeframes)
+
+        for i in range(min(len(dataset), 30)):
+            input, label, waveform, audio_length, audio_file_path = dataset[i]
+
+            print(input.shape[1], input.shape[2])
+            self.assertEqual(input.shape[1], dataset.n_mels)
+            self.assertEqual(input.shape[2], int(np.ceil(dataset.max_audio_length_sec * dataset.sample_rate / dataset.hop_length)))
+
+    def test_transform_spectrogram_plot(self):
+        # purpose: test that the transform is applied correctly
+
+        csv_df = data_processing.read_csv(
+            pd.read_csv(
+                os.path.join(BASE_DIR, 'data/metadata/dataset.csv'),
+                index_col=False
+            ),
+            BASE_DIR
+        )
+
+        dataset = AudioEmotionDataset(csv_df, max_audio_length_sec=2.7, test_mode=True)
+
+        for i in range(min(len(dataset), 20)):
+            input, label, waveform, audio_length, audio_file_path = dataset[i]
+
+            # Plot the spectrogram
+            plt.figure(figsize=(10, 4))
+            plt.imshow(input[0].numpy(), cmap='hot', origin='lower', aspect='auto')
+            plt.title('Mel Spectrogram')
+            plt.colorbar(format='%+2.0f dB')
+            plt.tight_layout()
+            plt.pause(1)  # display each plot for 1 seconds
+
 
 
 if __name__ == '__main__':
